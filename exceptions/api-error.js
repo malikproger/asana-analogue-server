f1ce06bdcf4539a714e5cb78961656a3016661cb
Malik <malik28067@mail.ru>
2024-07-28 23:50:24 +0300

    feat: added Unauthorized Error

--- exceptions/api-error.js
+++ exceptions/api-error.js
@@ -8,6 +8,10 @@ class ApiError extends Error {
     this.errors = errors;
   }
 
+  static UnauthorizedError() {
+    return new ApiError(401, 'Пользователь не авторизован');
+  }
+
   static BadRequest(message, errors = []) {
     return new ApiError(400, message, errors);
   }
