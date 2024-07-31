9cb1fab3852836df679ffebfcc4da5cf22d83b91
Malik <malik28067@mail.ru>
2024-07-31 18:57:42 +0300

    feat: task dtos arranged

--- services/task-service.js
+++ services/task-service.js
@@ -1,3 +1,4 @@
+const TaskDto = require('../dtos/task-dto');
 const { Task } = require('../models');
 
 class TaskService {
@@ -8,17 +9,20 @@ class TaskService {
 
   async createTask({ name, description, isCompleted, userId }) {
     const createdTask = await Task.create({ name, description, isCompleted, userId });
-    return createdTask;
+    const createdTaskDto = new TaskDto(createdTask);
+    return createdTaskDto;
   }
 
   async deleteTask(id) {
     const deletedTask = await Task.destroy({ where: { id } });
-    return deletedTask;
+    const deletedTaskDto = new TaskDto(deletedTask);
+    return deletedTaskDto;
   }
 
   async updateTask(task) {
     const updatedTask = await Task.update(task, { where: { id: task.id } });
-    return updatedTask;
+    const updatedTaskDto = new TaskDto(updatedTask);
+    return updatedTaskDto;
   }
 }
 
